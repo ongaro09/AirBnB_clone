8e8aad5a953050aab2cf8f468a9620fb9e12bfc0
ongaro09 <ongaromark7@gmail.com>
2024-02-12 00:25:52 +0000

    Defines the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines the BaseModel class, which serves as a foundation
-for other models within the AirBnB project.
+Defines the BaseModel class which serves as the foundation for other models
+within the AirBnB project.
 """
 
 from datetime import datetime
@@ -10,7 +10,8 @@ from uuid import uuid4
 
 class BaseModel:
     """
-    The BaseModel class defines common attributes and methods for other classes.
+    Represents the BaseModel class, providing common attributes and methods
+    for other classes in the AirBnB project.
     """
 
     def __init__(self, *args, **kwargs):
@@ -18,17 +19,20 @@ class BaseModel:
         Initializes an instance of the BaseModel class.
 
         Attributes:
-            id (str): Assigns a universally unique identification (id) to the object
-            created_at (datetime): Assigns the current datetime when the instance is created
-            updated_at (datetime): Assigns the current datetime when the instance is created
-                and will be updated every time you change the object
+            id (str): A universally unique identification (ID) assigned to the object.
+            created_at: Represents the timestamp of when the instance is created.
+            updated_at: Represents the timestamp of when the instance is updated.
         """
-        if kwargs:
-            for attribute, value in kwargs.items():
-                if attribute == "created_at" or attribute == "updated_at":
-                    setattr(self, attribute, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+        if kwargs is not None and kwargs != {}:
+            for attribute in kwargs:
+                if attribute == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif attribute == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    setattr(self, attribute, value)
+                    self.__dict__[attribute] = kwargs[attribute]
         else:
             from models import storage
             self.id = str(uuid4())
@@ -37,17 +41,23 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """Returns a string representation of an instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ Returns a string representation of the instance """
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates the instance attribute updated_at with the current datetime."""
+        """ Updates the instance's 'updated_at' attribute with the current datetime """
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of the BaseModel instance."""
+        """
+        Serializes the instance into a dictionary representation.
+
+        Returns:
+            dict: A dictionary containing all keys and values of the object.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
